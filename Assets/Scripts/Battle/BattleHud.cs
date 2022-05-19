793463665540a6aee388a025de15f4fadd1f92cd
shinja <rafa.issao@hotmail.com>
2022-05-19 19:15:44 -0300

    Initial commit

---
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BattleHud : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI levelText;
+    [SerializeField] HPBar hpBar;
+
+    Pokemon _pokemon;
+
+    public void SetData(Pokemon pokemon)
+    {
+        _pokemon = pokemon;
+
+        nameText.text = pokemon.Base.Name;
+        levelText.text = "Lvl " + pokemon.Level;
+
+        hpBar.SetHP((float)pokemon.HP / pokemon.MaxHp);
+
+    }
+
+    public IEnumerator UpdateHP()
+    {
+        yield return hpBar.SetHPSmooth((float)_pokemon.HP / _pokemon.MaxHp);
+    }
+}
+
